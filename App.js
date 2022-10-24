398d71ad3ca88583b029f2d10546e14c5f9a69dd
peterkfulweiler <85901984+peterkfulweiler@users.noreply.github.com>
2022-10-24 14:44:58 -0400

    init

--- App.js
+++ App.js
@@ -1,12 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { Camera, CameraType } from 'expo-camera';
+import { useEffect, useRef, useState } from 'react';
+import * as MediaLibrary from 'expo-media-library';
 export default function App() {
+  let cameraRef = useRef();
+  const [hasCameraPermission, setHasCameraPermission] = useState();
+  const [type, setType] = useState(CameraType.front);
+  //const [hasMediaLibraryPermission, setHasMediaLibraryPermission] = useState();
+
+  useEffect(() => {
+    (async () => {
+      const cameraPermission = await Camera.requestCameraPermissionsAsync();
+      //const hasMediaLibraryPermission = await MediaLibrary.requestPermissionsAsync();
+      setHasCameraPermission(cameraPermission.status === "granted");
+      //setHasMediaLibraryPermission(hasMediaLibraryPermission.status === "granted")
+    })();
+  }, [])
+
+  if (hasCameraPermission === undefined) {
+    return <Text> Requesting permissions...</Text>
+  }
+  else if (!hasCameraPermission) {
+    return <Text> Permissions for camera not granted. Change in Settings to Access App</Text>
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Camera style={styles.container} type={type}>
+   </Camera>
   );
 }
 
